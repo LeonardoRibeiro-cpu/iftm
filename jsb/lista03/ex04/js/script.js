d8340492744a36ad99ab87e2d49da3488a2146a9
LeonardoRibeiro-cpu <leonardo.ribeiro@estudante.iftm.edu.br>
2025-09-21 22:16:50 -0300

    Update script.js

--- jsb/lista03/ex04/js/script.js
+++ jsb/lista03/ex04/js/script.js
@@ -2,21 +2,18 @@ btn = document.getElementById("btnResultado");
 txt1 = document.getElementById("txtName1");
 txt2 = document.getElementById("txtName2");
 
-
 btn.addEventListener("click", exibir);
 
-
-function exibir(){
-    if(txt1.value.trim() =="" || txt2.value.trim() =="")
+function exibir() {
+  if (txt1.value.trim() == "" || txt2.value.trim() == "")
     alert("informe um numero");
-    else if (isNaN(txt1.value) || isNaN(txt2.value))
+  else if (isNaN(txt1.value) || isNaN(txt2.value))
     alert("Invalido! digite apenas numeros");
-   else{
-      soma = parseFloat(txt1.value) + parseFloat(txt2.value);
-      if(soma < 60)
-      alert("Reprovado")
-      else
-      alert("Aprovado")
-   }
-
+  else if (txt1.value > 50 || txt2.value > 50)
+    alert("Numero Invalido! Digite apenas numeros de 0 a 50");
+  else {
+    soma = parseFloat(txt1.value) + parseFloat(txt2.value);
+    if (soma < 60) alert("Reprovado");
+    else alert("Aprovado");
+  }
 }
