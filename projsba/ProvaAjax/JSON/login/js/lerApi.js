7270d36b0086ed75b14391a9876bb905e14796d5
Leonardo Ribeiro <leonardo.ribeiro@estudante.iftm.edu.br>
2026-03-24 21:31:22 -0300

    Delete projsba/ProvaAjax/JSON/login directory

--- projsba/ProvaAjax/JSON/login/js/lerApi.js
+++
@@ -1,8 +0,0 @@
-
-const lerArquivoJson = async() =>{
- const url = "https://leonardoribeiro-cpu.github.io/iftm/projsba/ProvaAjax/JSON/AjaxProva.json";
- const resposta = await fetch(url);
- const login = await resposta.json();
- return login;
-};
-export default lerArquivoJson;
